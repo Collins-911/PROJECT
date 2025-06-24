8f753d708c3e6c52cbab535458587ca7dd25fdd6
CollinsNugi <emelumbacollins@gmail.com>
2025-06-24 13:11:55 +0100

    third fix

--- src/pages/Purchased.jsx
+++ src/pages/Purchased.jsx
@@ -1,7 +1,7 @@
 import Sidebar from '../components/Sidebar';
 import Top from '../components/Top';
 import '../css/purchased.css';
-import Navigation from '../components/navigation';
+import Navigation from '../components/Navigation';
 
 export default function Purchased() {
 
